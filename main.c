65b1f09c1b3ce3052588aa8dec8c04730edc410f
Александр <zayzsacha@mail.ru>
2017-02-18 13:03:44 +0700

    Checking for correctness(2)

--- main.c
+++ main.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 
+int proverka(int srok, int vklad, int prov){
+	if (srok > 365){
+		prov++;
+	}
+	if (vklad < 10000){
+		prov++;
+	}
+	return prov;
+}
+
 int main()
 {
-	int srok, vklad;
+	int srok, vklad, prov = 0, b = 0;
 	printf("Введите срок: ");
 	scanf("%d", &srok);
 	printf("Введите вклад: ");
 	scanf("%d", &vklad);
 
+	prov = proverka(srok, vklad, prov);
+
+	
+	printf("%d", prov);
 	return 0;
 }
