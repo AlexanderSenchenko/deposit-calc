973e2005ba3851367ec3339837cfda783c1bc0c7
Александр <zayzsacha@mail.ru>
2017-02-25 13:10:19 +0700

    Deleted argument

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-int proverka(int srok, int vklad, int prov){
+int proverka(int srok, int vklad){
 	const int max_srok = 365, min_vklad = 10000;
+	int prov = 0;
 	if (srok > max_srok){
 		prov++;
 	}
@@ -13,13 +14,13 @@ int proverka(int srok, int vklad, int prov){
 
 int main()
 {
-	int srok, vklad, prov = 0;
+	int srok, vklad, prov;
 	printf("Введите срок: ");
 	scanf("%d", &srok);
 	printf("Введите вклад: ");
 	scanf("%d", &vklad);
 
-	prov = proverka(srok, vklad, prov);
+	prov = proverka(srok, vklad);
 
 	while (prov != 0){
 		if (prov == 11){
@@ -28,19 +29,19 @@ int main()
 			scanf("%d", &srok);
 			printf("Введите вклад еще раз: ");
 			scanf("%d", &vklad);
-			prov = proverka(srok, vklad, prov);
+			prov = proverka(srok, vklad);
 		}
 		if (prov == 1){
 			prov = 0;
 			printf("Введите срок еще раз: ");
 			scanf("%d", &srok);
-			prov = proverka(srok, vklad, prov);
+			prov = proverka(srok, vklad);
 		}
 		if (prov == 10){
 			prov = 0;
 			printf("Введите вклад еще раз: ");
 			scanf("%d", &vklad);
-			prov = proverka(srok, vklad, prov);
+			prov = proverka(srok, vklad);
 		}
 	}
 	printf("%d", prov);
